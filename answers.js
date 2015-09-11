9ab9c5eb29bd1b50d1d09cc2c224ce4b1572553f
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2015-09-11 18:53:25 +0000

    function lastCharacter

--- answers.js
+++ answers.js
@@ -1,11 +1,12 @@
-// defining a function that give the first character of the argument
+// defining a function that returns the first character of the argument
 function firstCharacter (string) {
 	return string[0];
 }
 
-firstCharacter("hello");
-
-
+// defining a function that returns the last character of the argument
+function lastCharacter (string) {
+    return string[string.length - 1];
+}
 
 
 
