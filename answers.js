5f851e6b5ae6213f55810a6534cff8e57fa4a039
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2015-09-11 21:05:22 +0000

    Changes in the explication of + y * and added the function stringTellsWhatToDo

--- answers.js
+++ answers.js
@@ -16,13 +16,35 @@ function GiveACharacter (str, num) {
 }
 
 //Defining a function that takes two numbers and adds them together
-// If I pass something other than a number to the function, that tells me that the argument is not defined
+// If I pass something other than a number to the function, it will return both argument writing together. 
 function addition (num1, num2) {
     return num1 + num2;
 }
 
 //Defining a function that takes two numbers and multiplies them together
-// If I pass something other than a number to the function, that tells me that the argument is not defined
+// If I pass something other than a number to the function, it will return NaN (Not a Number)
 function multiplication (num1, num2) {
     return num1 * num2;
+}
+
+
+/*Defining a function that takes two numbers and a string.
+It's the string that tells which arithmetic operators we have to use with the two numbers. 
+If there is no string, it returns "0".*/
+function stringTellsWhatToDo (num1, num2, str) {
+    if (str === "add") {
+        return num1 + num2;
+    }
+    if (str === "subtract") {
+        return num1 - num2;
+    }
+    if (str === "mult") {
+        return num1 * num2;
+    }
+    if (str === "div") {
+        return num1 / num2;
+    }
+    else {
+        return 0;
+    }
 }
