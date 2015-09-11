1b5635d1c07cf9116663945aba393a1daa210a56
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2015-09-11 20:15:20 +0000

    function that returns the character at the position represented by the number

--- answers.js
+++ answers.js
@@ -1,12 +1,16 @@
 // defining a function that returns the first character of the argument
-function firstCharacter (string) {
-	return string[0];
+function firstCharacter (str) {
+	return str[0];
 }
 
 // defining a function that returns the last character of the argument
-function lastCharacter (string) {
-    return string[string.length - 1];
+function lastCharacter (str) {
+    return str[str.length - 1];
 }
 
-
-
+// Defining a function that returns the character at the position represented by the number.
+function GiveACharacter (str, num) {
+    if (num >=0) {
+        return str[num];
+    }
+}
