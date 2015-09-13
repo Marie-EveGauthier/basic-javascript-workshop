3be40a77d0b896bac7b760d18d4de59d00a18668
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2015-09-13 19:10:58 +0000

    created function that takes a string, and returns the reverse of that string

--- answers.js
+++ answers.js
@@ -100,4 +100,13 @@ var stringRepeatedImproved = function(str, num) {
 }
 
 
+// Defining a function that takes a string, and returns the reverse of that string
+// I found the "recipe" of that function in : http://eddmann.com/posts/ten-ways-to-reverse-a-string-in-javascript/
+
+function reverse(s) {
+  var o = '';
+  for (var i = s.length - 1; i >= 0; i--)
+    o += s[i];
+  return o;
+}
  
